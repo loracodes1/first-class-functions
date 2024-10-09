b4675963c12d4b8165900a20c059d6a84fc8aa6d
florence <loracodes3@gmail.com>
2024-10-09 14:18:15 +0300

    store functions in data structures

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ const greet = function(name) {
     return `Hello, ${name}!`;
 };
 console.log(greet("Lora")); // Output: Hello, Lora!
+
+//STORE FUNCTIONS IN DATA STRUCTURES
+// Storing functions in an array
+const functionArray = [
+    function() { return "Function 1"; },
+    function() { return "Function 2"; },
+    function() { return "Function 3"; }
+];
+
+console.log(functionArray[0]()); // Output: Function 1
+
+// Storing functions in an object
+const functionObject = {
+    func1: function() { return "Hello from func1"; },
+    func2: function() { return "Hello from func2"; }
+};
+
+console.log(functionObject.func1()); // Output: Hello from func1
+
